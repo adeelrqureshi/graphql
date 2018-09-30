3a567bc5ef4de91f1b9f99642b0f14ba7ff5b68a
Adeel Qureshi <adeelq@pk.ibm.com>
2018-09-30 17:13:31 +0500

    Added Mutation to add new author

--- server/server.js
+++ server/server.js
@@ -36,6 +36,10 @@ const typeDefs = `
         getAuthors: [Author]
         retrieveAuthor(id: ID!) : Author
     }
+
+    type Mutation{
+        createAuthor (name: String! , gender: String! ): Author
+    }
 `;
 
 const resolvers = {
@@ -43,6 +47,24 @@ const resolvers = {
         getAuthors : () => authors,
         retrieveAuthor: (obj,{id}) => authors.find(author => author.id === id)
 
+    },
+
+    Mutation:{
+        createAuthor : (obj,args)=> {
+            const id = String(authors.length + 1);
+            const {name, gender} = args;
+
+            const newAuthor = {
+                id,
+                info: {
+                    name,
+                    gender
+                }
+            }
+
+            authors.push(newAuthor);
+            return newAuthor;
+        }
     }
 };
 
